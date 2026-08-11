5c1d640b8163d45e88b9cf88201b90989e0a4c54
Jin Young Kim <wlsdud66523@kakao.com>
2026-08-12 07:22:45 +0900

    双方向関連と関連のオーナー 1 - 基本

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Persistence;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Team;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -23,27 +25,22 @@ public class JpaMain {
 
             Member member = new Member();
             member.setName("손흥민");
-
-//            // 연관 관계를 참조가 아닌, 외래키로 관계 매핑을 해서 INSERT하면 생기는 문제점
-//            Long teamId = team.getId(); // team_id 조회를 위해서 getId()를 해줘야 한다 : 객체지향 스러운 것은 member.setTeam(team);
-//            member.setTeamId(teamId); // 외래키를 애플리케이션 코드에서 직접 사용하고 있다.
-//            entityManager.persist(member);
-//
-//
-//            // 연관 관계를 참조가 아닌, 외래키로 관계 매핑을 해서 SELECT하면 생기는 문제점
-//            // -> 객체의 자유로운 그래프 탐색이 안된다.
-//            Member findMember = entityManager.find(Member.class, member.getId());
-//            Long findTeamId = findMember.getTeamId();
-//            entityManager.find(Team.class, findTeamId); // 객체 지향 스로운 것은 그냥 Team.getMember()로 찾는 것이다.
-
-
-            // 연관 관계를 참조로 매핑해서 INSERT문 실행
-            member.setTeam(team); // 객체 지향성을 유지
+            member.setTeam(team);
             entityManager.persist(member);
+
+            entityManager.flush();
+            entityManager.clear();
+
             Member findMember = entityManager.find(Member.class, member.getId());
 
-            // 연관 관계를 참조로 매핑해서 SELECT문 실행
-            Team findTeam = member.getTeam(); // 객제 지향성을 유지
+            // mappedBy로 인해 이제 Team 객체에서도 Member 객체를 조회할 수 있게 되었다.
+            // -> 양방향 매핑이 된 것이다.
+            Team findTeam = findMember.getTeam();
+            List<Member> members = findTeam.getMembers();
+            for (Member m : members) {
+                System.out.println("member = " + m.getName());
+            }
+
 
             transaction.commit(); // 트랜잭션 종료
         } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Team.java
+++ src/main/java/jpabook/jpashop/domain/Team.java
@@ -1,10 +1,10 @@
 package jpabook.jpashop.domain;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +12,10 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team") // mappedby : 연관 관계 매핑 애노테이션이 걸려 있는 객체(List<Member>)가 그 객체의 어느 객체와 매핑이 걸려 있는지!!
+                                  // 이걸 통해 이제 Team 객체에서도 Member 객체를 조회할 수가 있다.
+    private List<Member> members = new ArrayList<>(); // members.add() 시 NUll포인터 예외 방지를 위해 미리 초기화 해둠
 
     public Long getId() {
         return id;
@@ -27,5 +31,9 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
     }
 }
