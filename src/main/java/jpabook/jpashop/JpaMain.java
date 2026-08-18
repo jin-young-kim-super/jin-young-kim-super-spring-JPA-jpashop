c0adfb932453ead2c335381d8461aa6df486a8e0
Jin Young Kim <wlsdud66523@kakao.com>
2026-08-19 05:50:00 +0900

    実践例 2 - 関連マッピング開始

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Team;
 
 import java.util.List;
 
@@ -19,34 +18,13 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("TEAM A");
-            entityManager.persist(team);
-
-            Member member = new Member();
-            member.setName("손흥민");
-            member.setTeam(team);
-            entityManager.persist(member);
-
-            entityManager.flush();
-            entityManager.clear();
-
-            Member findMember = entityManager.find(Member.class, member.getId());
-
-            // mappedBy로 인해 이제 Team 객체에서도 Member 객체를 조회할 수 있게 되었다.
-            // -> 양방향 매핑이 된 것이다.
-            Team findTeam = findMember.getTeam();
-            List<Member> members = findTeam.getMembers();
-            for (Member m : members) {
-                System.out.println("member = " + m.getName());
-            }
 
 
-            transaction.commit(); // 트랜잭션 종료
+            transaction.commit();
         } catch (Exception e) {
-            transaction.rollback(); // 트랜잭션 종료
+            transaction.rollback();
         } finally {
-            // STEP 4. DB 종료
+
             entityManager.close();
         }
         entityManagerFactory.close();
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,28 +3,21 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
-/**
- * Setter 사용은 디버깅을 어렵게 한다. 최대한 생성자에서 세팅을 끝내자.
- * -> 디버깅을 할 떄 setter 사용의경우 Member가 어느 코드에서 setter에 의해 값이 변경됐는지 일일이 찾아야 한다.
- * 반면 생성자를 통한 세팅의 경우, 생성자 한 곳만 보면 된다. 즉 유지보수성이 떨어 진다.
- */
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
     private Long id;
-
-    @Column(name = "USERNAME")
-
     private String name;
-    //@Column(name = "TEAM_ID")
-    //private Long teamId; // 참조가 아닌 외래키 사용!(RDB 설계에 맞춤)
+    private String city;
+    private String street;
+    private String zipcode;
 
-    // 연관 관계 매핑은 항상 DB입장에서 매핑을 해야 한다.
-    @ManyToOne // DB 입장에서는 Member 테이블에 대해 TEAM 테이블은 1의 관계
-    @JoinColumn(name="TEAM_ID") // DB에서는 연관 관계인 Member 테이블에서 TEAM 테이블의 어떤 키를 FK로 매핑할 지 정보가 필요
-    private Team team;          // DB에서는 반드시 多 테이블에 FK를 둔다.
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -42,19 +35,35 @@ public class Member {
         this.name = name;
     }
 
-    public Team getTeam() {
-        return team;
+    public String getCity() {
+        return city;
     }
 
-    public void changeTeam(Team team) {
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-        this.team = team;
-        /**
-         * 영한이 실무 팁 : Team.getMembers().add(member)이 부분은 연관 관계 주인 객체에서 편의 메서드 작성을 해 놓는다
-         * -> 인간은 반드시 Team.getMembers().add(member)를 까먹는다. 그 실수를 방지하게 위해 이렇게 편의 메서드를 만들어 놓는 걸 추천
-         */
-        team.getMembers().add(this);
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 }
+
+
+
+
+
+
 
 
